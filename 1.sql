7c903f9c856f99ced5798b8d237aa00ac3a3b806
Loic Ortola <loic.ortola@cpe.fr>
2013-09-10 15:26:25 +0200

    updated sql script

--- 1.sql
+++ 1.sql
@@ -1,7 +1,5 @@
-# --- First database schema
-
-# --- !Ups
-
+create schema `computer-database-db`;
+use `computer-database-db`;
 create table company (
   id                        bigint not null,
   name                      varchar(255),
@@ -17,25 +15,5 @@ create table computer (
   constraint pk_computer primary key (id))
 ;
 
-create sequence company_seq start with 1000;
-
-create sequence computer_seq start with 1000;
-
 alter table computer add constraint fk_computer_company_1 foreign key (company_id) references company (id) on delete restrict on update restrict;
 create index ix_computer_company_1 on computer (company_id);
-
-
-# --- !Downs
-
-SET REFERENTIAL_INTEGRITY FALSE;
-
-drop table if exists company;
-
-drop table if exists computer;
-
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists company_seq;
-
-drop sequence if exists computer_seq;
-
